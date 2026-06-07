b99ba7cb01f922097331c5f0bd966252645a38ed
abde1khaliq <abdekhaliqqq@gmail.com>
2026-06-07 21:00:08 +0300

    fix konda class

--- src/anakonda/core.py
+++ src/anakonda/core.py
@@ -1,19 +1,12 @@
-from .router import Router
-
 class Konda:
     def __init__(self, title=None, description=None):
         self.title = title
         self.description = description
-        self.router = Router()
 
     async def __call__(self, scope, receive, send):
-        for route in self.router.routes:
-            if route.matches(scope):
-                return await route.handler(scope, receive, send)
-
         await send({
             "type": "http.response.start",
-            "status": 404,
+            "status": 200,
             "headers": [
                 [b'content-type', b'text/plain']
             ]
@@ -21,5 +14,5 @@ class Konda:
 
         await send({
             "type": "http.response.body",
-            "body": b"Not Found"
+            "body": b"Hello World"
         })
