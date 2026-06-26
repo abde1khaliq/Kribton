7f36707e6f7a7d8b80f5112aa291416b172ac345
abde1khaliq <abdelkhaliqqq@gmail.com>
2026-06-26 19:25:11 +0300

    fix import bug

--- src/kribton/__init__.py
+++ src/kribton/__init__.py
@@ -1,7 +1,7 @@
-from kribton.core import Konda
+from kribton.core import Kribton
 from kribton.router import Router
 from kribton.route import Route
 from kribton.request import Request
 from kribton.response import Response
 
-__all__ = ["Konda", "Router", "Route", "Request", "Response"]
+__all__ = ["Kribton", "Router", "Route", "Request", "Response"]
